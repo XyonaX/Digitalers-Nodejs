b5f7593d8979d6b855ac140703b444275762e89c
Jonatan <jonatan.vargasportillo@gmail.com>
2023-07-20 16:35:42 -0300

    se agregaron un par de desafios

---
+++ desafio11/app.js
@@ -0,0 +1,35 @@
+//1)
+
+// let edad = parseInt(prompt("Ingrese su edad: "));
+
+// if(edad >= 18){
+//     console.log("Tienes permitido el ingreso");
+// }else{
+//     console.log("Tienes prohibido el ingreso");
+// }
+
+
+// //2)
+
+// let dni = false
+
+// if(dni === true){
+//     console.log("Tienes permitido el ingreso")
+// }else{
+//     console.log("No tienes permitido el ingreso")
+// }
+
+
+// //3)
+
+// let estadoDeConductor = "Designado"
+
+// if(estadoDeConductor === "Designado"){
+//     console.log("Tienes permitido conducir")
+// }else{
+//     console.log("No puedes conducir")
+// }
+
+
+//4)
+
